1be06e350ee3a7d66c906bea64db6d3edef20231
mbechtel2 <mbechtel@ku.edu>
2019-02-14 22:12:33 +0000

    Figure 11/13 script fixes

--- figures/Fig11/run.sh
+++ figures/Fig11/run.sh
@@ -6,8 +6,8 @@ echo "\tRunning baseline MemGuard"
 cd ../../base-memguard
 insmod memguard.ko g_hw_type=armv7
 echo mb 100 100 100 100 > /sys/kernel/debug/memguard/limit
-bandwidth -c 0 > ../figures/Fig12/base-bwr-results.txt 2>&1
-bandwidth -a write -c 0 > ../figures/Fig12/base-bww-results.txt 2>&1
+bandwidth -c 0 > ../figures/Fig11/base-bwr-results.txt 2>&1
+bandwidth -a write -c 0 > ../figures/Fig11/base-bww-results.txt 2>&1
 rmmod memguard
 
 #Modified MemGuard
@@ -16,6 +16,6 @@ cd ../memguard
 insmod memguard.ko
 echo mb 500 500 500 500 > /sys/kernel/debug/memguard/read_limit
 echo mb 100 100 100 100 > /sys/kernel/debug/memguard/write_limit
-bandwidth -c 0 > ../figures/Fig12/modified-bwr-results.txt 2>&1
-bandwidth -a write -c 0 > ../figures/Fig12/modified-bww-results.txt 2>&1
+bandwidth -c 0 > ../figures/Fig11/modified-bwr-results.txt 2>&1
+bandwidth -a write -c 0 > ../figures/Fig11/modified-bww-results.txt 2>&1
 rmmod memguard
--- figures/Fig13/run.sh
+++ figures/Fig13/run.sh
@@ -4,21 +4,21 @@ cd ../../memguard
 insmod memguard.ko
 
 #No regulation
-echo "Copying Figure 4 for no regulation results"
+echo "\tCopying Figure 4 for no regulation results"
 cp ../figures/Fig4/results.txt noreg-results.txt
 cd ../IsolBench/scripts
-
-#Read: 500MB/s, Write: 100MB/s
-echo "\t500R/100W"
-echo mb 2500 500 500 500 > /sys/kernel/debug/memguard/read_limit
-echo mb 2500 100 100 100 > /sys/kernel/debug/memguard/write_limit
-./test-isolbench_bwwrite.sh > ../../figures/Fig13/500r-100w-results.txt
 
 #Read: 1000MB/s, Write: 100MB/s
 echo "\t1000R/100W"
 echo mb 2500 1000 1000 1000 > /sys/kernel/debug/memguard/read_limit
 echo mb 2500 100 100 100 > /sys/kernel/debug/memguard/write_limit
 ./test-isolbench_bwwrite.sh > ../../figures/Fig13/1000r-100w-results.txt
+
+#Read: 500MB/s, Write: 100MB/s
+echo "\t500R/100W"
+echo mb 2500 500 500 500 > /sys/kernel/debug/memguard/read_limit
+echo mb 2500 100 100 100 > /sys/kernel/debug/memguard/write_limit
+./test-isolbench_bwwrite.sh > ../../figures/Fig13/500r-100w-results.txt
 
 #Read: 500MB/s, Write: 50MB/s
 echo "\t500R/50W"
